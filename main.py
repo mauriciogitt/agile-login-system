80b19ff5e54c58681335ff08a5134744eb80f0d3
Mauricio <mauriciogit7@gmail.com>
2026-05-29 03:35:44 -0300

    Create login system

---
+++ main.py
@@ -0,0 +1,33 @@
+usuario_correto = "admin"
+senha_correta = "123"
+
+tarefas = []
+
+usuario = input("Usuário: ")
+senha = input("Senha: ")
+
+if usuario == usuario_correto and senha == senha_correta:
+
+    while True:
+
+        print("\n1 - Adicionar tarefa")
+        print("2 - Ver tarefas")
+        print("3 - Sair")
+
+        opcao = input("Escolha: ")
+
+        if opcao == "1":
+
+            tarefa = input("Digite a tarefa: ")
+            tarefas.append(tarefa)
+
+        elif opcao == "2":
+
+            print(tarefas)
+
+        elif opcao == "3":
+
+            break
+
+else:
+    print("Login inválido")
